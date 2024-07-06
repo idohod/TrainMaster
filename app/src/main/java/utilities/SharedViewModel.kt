b2a060527dbafd52a468867b6012943242bfb197
idohod <127017712+idohod@users.noreply.github.com>
2024-07-06 11:52:52 +0300

    init

--- app/src/main/java/utilities/SharedViewModel.kt
+++ app/src/main/java/utilities/SharedViewModel.kt
@@ -6,7 +6,5 @@ import androidx.lifecycle.ViewModel
 class SharedViewModel : ViewModel() {
     val traineeName = MutableLiveData<String>()
     val fromTimer = MutableLiveData<Boolean>()
-val staqm =null
-
 
 }
